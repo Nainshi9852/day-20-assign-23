b2710ad5aed1fa47862de3a41a5930e4773c560d
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-08-04 15:41:24 +0530

    First Commit

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MysharedLibrary;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("User Registeration");
+                UserModule.RegisterUser();
+
+                Console.WriteLine("Do you wish to continue or if yes press 'y' or any other key");
+                string choice = Console.ReadLine();
+                if (choice == "y")
+                {
+                    Console.WriteLine("Registration continues");
+                }
+                else
+                {
+                    break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
